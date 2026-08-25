44ee1320f29e9307c56c9f8e00d475ab4e094a22
binapalawan-collab <binapalawan@gmail.com>
2026-08-25 15:10:47 +0800

    fix(ci): scope root lint to contractor suite

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage'] },
+  { ignores: ['dist', 'coverage', 'apps'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -13,4 +13,3 @@ export default tseslint.config(
     },
   },
 )
-
